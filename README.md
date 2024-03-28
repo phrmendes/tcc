e91be43f1e7df92ee36fa2c6301b8497cf4272b4
Pedro Mendes <pedrohrmendes@proton.me>
2024-03-27 23:27:56 -0300

    Update dependencies.

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 My economics bachelor's thesis (UFABC)!
 
+# Dependencies
+
+- [Quarto](https://quarto.org)
+- [Tectonic](https://tectonic-typesetting.github.io)
+- [Python](https://www.python.org)
+- [Poetry](https://python-poetry.org)
+- [Docker](https://www.docker.com) or [Podman](https://podman.io)
+
 # Abstract
--- flake.nix
+++ flake.nix
@@ -1,28 +1,47 @@
 {
-  description = "Dev environment";
+  description = "Thesis environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
-    flake-utils,
+    utils,
     nixpkgs,
   }:
-    flake-utils.lib.eachDefaultSystem (
+    utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
         fhs = pkgs.buildFHSUserEnv {
-          name = "pixi";
+          name = "thesis";
+          targetPkgs = pkgs: (with pkgs; [micromamba]);
+          profile = ''
+            export MAMBA_ROOT_PREFIX="./.mamba"
+            export DEPENDENCIES
 
-          targetPkgs = pkgs: (with pkgs; [
-            pixi
-          ]);
+            eval "$(micromamba shell hook --shell=posix)"
+
+            if [[ -f "./env.yml" ]]; then
+              DEPENDENCIES="-f env.yml"
+            fi
+
+            if [[ -f "./conda-lock.yml" ]]; then
+              DEPENDENCIES+=" -f conda-lock.yml"
+            fi
+
+            if [[ -d "./.mamba" ]]; then
+              micromamba update $DEPENDENCIES --yes
+            else
+              micromamba create $DEPENDENCIES --yes
+            fi
+
+            micromamba activate thesis
+          '';
         };
       in {
         devShell = fhs.env;
