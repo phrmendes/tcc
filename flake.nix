f33e52618c0677280244a185442fdd41ec59fca6
Pedro Mendes <pedrohrmendes@proton.me>
2024-04-07 20:47:34 -0300

    Update tex path.
    Update dependencies.
    
    Update dependencies.
    
    Update dependencies.

--- flake.nix
+++ flake.nix
@@ -3,22 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
-    self,
-    utils,
+    flake-utils,
     nixpkgs,
+    self,
   }:
-    utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
           config = {
             allowUnfree = true;
             cudaSupport = true;
-            allowBroken = true;
           };
         };
         fhs = pkgs.buildFHSUserEnv {
@@ -28,7 +27,6 @@
             export LD_LIBRARY_PATH=${cudaPackages.cudatoolkit.lib}/lib:$LD_LIBRARY_PATH
             export LD_LIBRARY_PATH=${cudaPackages.cudatoolkit}/lib:$LD_LIBRARY_PATH
             export LD_LIBRARY_PATH=${cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
-            export LD_LIBRARY_PATH=${cudaPackages.tensorrt}/lib:$LD_LIBRARY_PATH
             export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
             export LD_LIBRARY_PATH=${zlib}/lib:$LD_LIBRARY_PATH
           '';
@@ -36,8 +34,8 @@
           runScript = ''poetry shell'';
 
           targetPkgs = pkgs: (with pkgs; [
+            duckdb
             poetry
-            pyright
             python312
             quarto
             ruff
